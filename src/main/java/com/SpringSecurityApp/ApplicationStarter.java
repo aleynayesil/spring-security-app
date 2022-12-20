014096f4f03945243792c458134b00ca65a9658a
aleynayesil <aleyna.yesil@freightest.com>
2022-12-20 18:07:52 +0300

    Initial Commit

--- src/main/java/com/SpringSecurityApp/ApplicationStarter.java
+++ src/main/java/com/SpringSecurityApp/ApplicationStarter.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringSecurityAppApplication {
+public class ApplicationStarter {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringSecurityAppApplication.class, args);
+		SpringApplication.run(ApplicationStarter.class, args);
 	}
 
 }
